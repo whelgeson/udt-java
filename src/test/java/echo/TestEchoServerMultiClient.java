e19d67d41477424996c9adfd6dd8f9852eaf6360
Guillaume Anthouard <guillaume.anthouard@hotmail.fr>
2020-12-10 10:05:51 +0100

    Ports tests to junit 5, some test are not working because of a bind exception ?

--- src/test/java/echo/TestEchoServerMultiClient.java
+++ src/test/java/echo/TestEchoServerMultiClient.java
@@ -1,44 +1,45 @@
 package echo;
 
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-
-import junit.framework.Assert;
-
-import org.junit.Test;
-
+import org.junit.jupiter.api.Test;
 import udt.UDTClient;
 import udt.util.Util;
 
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class TestEchoServerMultiClient {
 
-	@Test
-	public void testTwoClients()throws Exception{
-		EchoServer es=new EchoServer(65321);
-		es.start();
-		Thread.sleep(1000);
-		
-		UDTClient client=new UDTClient(InetAddress.getByName("localhost"),12345);
-		client.connect("localhost", 65321);
-		doClientCommunication(client);
-		
-		UDTClient client2=new UDTClient(InetAddress.getByName("localhost"),12346);
-		client2.connect("localhost", 65321);
-		doClientCommunication(client2);
-		
-		es.stop();
-	}
+    @Test
+    public void testTwoClients() throws Exception {
+        EchoServer es = new EchoServer(65321);
+        es.start();
+        Thread.sleep(1000);
 
-	private void doClientCommunication(UDTClient client)throws Exception{
-		PrintWriter pw=new PrintWriter(new OutputStreamWriter(client.getOutputStream(),"UTF-8"));
-		pw.println("test");
-		pw.flush();
-		System.out.println("Message sent.");
-		client.getInputStream().setBlocking(false);
-		String line=Util.readLine(client.getInputStream());
-		Assert.assertNotNull(line);
-		System.out.println(line);
-		Assert.assertEquals("test",line);
-	}
+        UDTClient client = new UDTClient(InetAddress.getByName("localhost"), 12345);
+        client.connect("localhost", 65321);
+        doClientCommunication(client);
+
+        UDTClient client2 = new UDTClient(InetAddress.getByName("localhost"), 12346);
+        client2.connect("localhost", 65321);
+        doClientCommunication(client2);
+
+        es.stop();
+    }
+
+    private void doClientCommunication(UDTClient client) throws Exception {
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8));
+        pw.println("test");
+        pw.flush();
+        System.out.println("Message sent.");
+        client.getInputStream().setBlocking(false);
+        String line = Util.readLine(client.getInputStream());
+        assertNotNull(line);
+        System.out.println(line);
+        assertEquals("test", line);
+    }
 }
